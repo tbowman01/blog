83acba315d6d21f7ac186c3a3cb538422a4a2cb8
tbowman01 <tbowman01@users.noreply.github.com>
2019-09-12 10:32:55 -0400

    Update BlogPost.js

--- src/resources/BlogPost.js
+++ src/resources/BlogPost.js
@@ -3,7 +3,7 @@ export default {
     return {
       path: `/object/${id}`,
       resolve: (response, mappers) => {
-        let { title, content, created_at, metadata } = response.object
+        let { title, content, Created_at, metadata } = response.object
         let meta = {
           description: metadata.description,
           published: created_at,
